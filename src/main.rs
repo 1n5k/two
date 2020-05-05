cf7b7f1f9bfbabf779e7dd5a4ca8193f898a0ae7
Jinsuke <jnsk@jnsk.info>
2020-05-05 21:44:42 +0900

    Add test

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@
 
 use rocket_contrib::serve::StaticFiles;
 
-
 fn rocket() -> rocket::Rocket {
     rocket::ignite().mount("/", StaticFiles::from("static"))
 }
@@ -18,11 +17,21 @@ mod test {
     use super::rocket;
     use rocket::local::Client;
     use rocket::http::Status;
+    use rocket::http::ContentType;
+    
+    #[test]
+    fn test_index() {
+        let client = Client::new(rocket()).unwrap();
+        let response = client.get("/").dispatch();
+        assert_eq!(response.status(), Status::Ok);        
+    }
 
     #[test]
-    fn test_hello() {
+    fn test_files() {
         let client = Client::new(rocket()).unwrap();
-        let mut response = client.get("/").dispatch();
-        assert_eq!(response.status(), Status::Ok);        
+        let mut response = client.get("/favicon.png").dispatch();
+        assert_eq!(response.headers().iter().next(), Some(ContentType::PNG.into()));
+        let mut response = client.get("/keybase.txt").dispatch();
+        assert_eq!(response.headers().iter().next(), Some(ContentType::Plain.into()));
     }
 }
