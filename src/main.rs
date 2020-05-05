7390959bfae6b19f4181b87f4081de3037394242
Jinsuke <jnsk@jnsk.info>
2020-05-05 15:47:08 +0900

    Add test

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,29 @@
 #[macro_use] extern crate rocket;
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, World?!"
+fn hello() -> &'static str {
+    "HELLo, world!"
+}
+
+fn rocket() -> rocket::Rocket {
+    rocket::ignite().mount("/", routes![hello])
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket().launch();
+}
+
+#[cfg(test)]
+mod test {
+    use super::rocket;
+    use rocket::local::Client;
+    use rocket::http::Status;
+
+    #[test]
+    fn test_hello() {
+        let client = Client::new(rocket()).unwrap();
+        let mut response = client.get("/").dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.body_string(), Some("HELLo, world!".into())); 
+    }
 }
